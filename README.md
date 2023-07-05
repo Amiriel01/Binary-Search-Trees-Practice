ca128b1e654dcbf3cc00934e859cc5e913d81a78
Sarah Quinney <quinney.sarah@gmail.com>
2023-07-05 15:40:40 -0500

    built node, tree, buildtree functions

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Binary-Search-Trees-Practice
 Work in Progress
+
+**Objective**
+
+The goal is to practice writing Bianary Search Trees. 
+
+The Bianary Search Tree includes:
+
+1. I will practice building a Node class / factory. That has an attribute for the data it stores as well as its left and right children.
+
+2. I will practice building a Tree class / factory which accepts an array when initialized. The Tree class will have a root attribute which uses the return value of buildTree function.
+
+
---
+++ index.js
@@ -0,0 +1,56 @@
+//function that prints balanced bst in terminal//
+const prettyPrint = (node, prefix = '', isLeft = true) => {
+    if (node === null) {
+        return;
+    }
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
+//node factory that has attribute for the data it stores and left/right children//
+class node {
+    //if data, left, or right has no data the value will return null//
+    constructor(data = null, left = null, right = null) {
+        //if data, left, or right has a value it will overwrite the null data//
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+};
+
+class tree {
+    //input array is input from the user//
+    constructor(inputArray) {
+        //calls the build tree function and inputs the inputArray(user input), start(location 0), and end(inputArray.length -1 is the end of the array) parameters from the function//
+        this.root = this.buildTree(inputArray, 0, inputArray.lengh - 1);
+        //will input the value from the root in the prettry print function to print it in a bst format//
+        prettyPrint(this.root);
+    }
+
+    //recursive function with the inputArray, start, and end parameters//
+    buildTree(inputArray, start, end) {
+        //it will be null if the scanned array is complete and the root is flipped the opposite direction//
+        if (start > end) return null;
+        //mid is the average of start and end//
+        let mid = parseInt((start + end) / 2);
+        //root is the inputArray mid value found above//
+        let root = new node(inputArray[mid]);
+
+        //the left side is found by taking the array start and going to the left of mid (-1)//
+        root.left = this.buildTree(inputArray, start, mid - 1);
+        //the right is found by taking the array and going to the right from the mid (+1) to the end//
+        root.right = this.buildTree(inputArray, mid + 1, end);
+        //return the root value//
+        return root;
+    }
+};
+//create test array variable so you only have to change it once if you change the array values//
+let testInputArray = [1, 2, 3, 4, 5, 6, 7];
+//take the testInputArray and start at 1 and end at 7//
+balancedBST = new tree (testInputArray, 1, 7);
+
